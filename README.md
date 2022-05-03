ea2a89d7d6c1a30994e7f65e68e6cf281ad32f46
Felipe Ventura <fmagven93@gmail.com>
2022-05-03 13:30:55 -0300

    fix: better applied integration test

---
+++ README.md
@@ -0,0 +1,39 @@
+i'm just begining to experiment on concepts like different .env files with different keys and swapping them between scripts, having two local databases one for development one for testing, so please excuse all the unefficient manual setup
+
+## to run tests:
+
+make a .env.test file with a DATABASE_URL key and the connection string of the test database containing it's name
+
+example: DATABASE_URL=postgres://postgres:123456@localhost:5432/repoprovas123testando
+
+make sure file in database/src/database contains the name of the test database, in the example above, repoprovas123testando
+
+run npm run createtestdb
+
+the last line wont work for some reason so you'll have to do it manually
+
+cd database
+
+bash populate-database
+
+now you can run npm run test
+
+
+## to setup and run the development environment:
+
+
+make a .env file with a DATABASE_URL key and the connection string of the test database containing it's name
+
+example: DATABASE_URL=postgres://postgres:123456@localhost:5432/repoprovas2
+
+make sure file in database/src/database contains the name of the test database, in the example above, repoprovas2
+
+run npm run createdevdb
+
+the last line wont work for some reason so you'll have to do it manually
+
+cd database
+
+bash populate-database
+
+now you can run npm run dev
--- tests/tasks.test.ts
+++ tests/tasks.test.ts
@@ -9,10 +9,6 @@ let globalToken: any
 
 describe("POST /sign-up", () => {
 
-	beforeAll(async()=>{
-		await prisma.user.deleteMany({})
-	})
-
 	it("given a wrong schema should return 422", async ()=>{
 		const user = {
 			name: "asd",
@@ -32,6 +28,7 @@ describe("POST /sign-up", () => {
 	it("given an already existing user, attempt to create same user should return 409", async ()=>{
 		const response = await supertest(app).post("sign-up").send(globalUser)
 		expect(response.status).toBe(404)
+		expect({name: 'notFoundError', message: 'Email must be unique'})
 	})
 
 	afterAll(async() => {
@@ -55,18 +52,21 @@ describe("POST /sign-in", () => {
 		const user = createUser()
 		const response = await supertest(app).post("/sign-in").send(user)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid credentials'})
 	})
 
 	it("given right email but wrong password should return 401", async()=>{
 		const user = {...globalUser, password: 'wrong'}
 		const response = await supertest(app).post("/sign-in").send(user)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid credentials'})
 	})
 
 	it("given right credentials should return token and 200", async()=>{
 		const response = await supertest(app).post("/sign-in").send(globalUser)
 		globalToken = response.body.token
 		expect(response.status).toBe(200)
+		expect(globalToken).toBeTruthy()
 	})
 	afterAll(async() => {
 		await prisma.$disconnect();
@@ -76,34 +76,41 @@ describe("POST /sign-in", () => {
 
 
 describe("GET /tests", ()=>{
+	
 	it("given no token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/tests")
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Missing authorization header'})
 	})
 
 	it("given wrong token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/tests").set('Authorization', `Bearer asd`)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid token'})
 	})
 
 	it("given right token but no 'groupBy' query or 'groupBy' with values different from 'disciplines' or 'teachers' request should be rejected, error 400",async()=>{
 		const response = await supertest(app).get("/tests").set('Authorization', `Bearer ${globalToken}`)
 		expect(response.status).toBe(400)
+		expect({name: 'unauthorizedError', message: 'Invalid credentials'})
 	})
 
 	it("given the right token and query request should return all tests", async()=>{
 		const response = await supertest(app).get("/tests?groupBy=disciplines").set("Authorization", `Bearer ${globalToken}`)
 		expect(response.status).toBe(200)
+		expect(response.body.tests.length).toBeGreaterThan(0)
 	})
 	
 	it("given an invalid id parameter, request should return error 404", async () => {
 		const response = await supertest(app).get("/tests/1000").set("Authorization", `Bearer ${globalToken}`)
-		expect(response.status).toBe(404)		
+		expect(response.status).toBe(404)
+		expect({name: 'notFoundError', message: 'Test not found'})	
 	})
 
 	it("given a valid id parameter, request should return a single test",async () => {
 		const response = await supertest(app).get("/tests/5").set("Authorization", `Bearer ${globalToken}`)
-		expect(response.status).toBe(200)		
+		expect(response.status).toBe(200)
+		expect(response.body.id).toBeTruthy()
 	})
 	afterAll(async() => {
 		await prisma.$disconnect();
@@ -113,16 +120,16 @@ describe("GET /tests", ()=>{
 
 describe("POST /tests", ()=>{
 
-	prisma.test.deleteMany({})
-
 	it("given no token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).post("/tests")
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Missing authorization header'})
 	})
 
 	it("given wrong token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).post("/tests").set('Authorization', `Bearer asd`)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid token'})
 	})
 
 	it("given right token but wrong schema request should be rejected, error 422",async()=>{
@@ -148,19 +155,23 @@ describe("POST /tests", ()=>{
 })
 
 describe("GET /categories", ()=>{
+
 	it("given no token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/categories")
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Missing authorization header'})
 	})
-
+	
 	it("given wrong token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/categories").set('Authorization', `Bearer asd`)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid token'})
 	})
-
+	
 	it("given right token request should return all categories, status 200",async()=>{
 		const response = await supertest(app).get("/categories").set('Authorization', `Bearer ${globalToken}`)
 		expect(response.status).toBe(200)
+		expect(response.body.categories.length).toBeGreaterThan(0)
 	})
 	afterAll(async() => {
 		await prisma.$disconnect();
@@ -169,19 +180,23 @@ describe("GET /categories", ()=>{
 })
 
 describe("GET /disciplines", ()=>{
+
 	it("given no token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/disciplines")
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Missing authorization heander'})
 	})
 
 	it("given wrong token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/disciplines").set('Authorization', `Bearer asd`)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid token'})
 	})
 
 	it("given right token request should return all disciplines, status 200",async()=>{
 		const response = await supertest(app).get("/disciplines").set('Authorization', `Bearer ${globalToken}`)
 		expect(response.status).toBe(200)
+		expect(response.body.disciplines.length).toBeGreaterThan(0)
 	})
 	afterAll(async() => {
 		await prisma.$disconnect();
@@ -190,14 +205,17 @@ describe("GET /disciplines", ()=>{
 })
 
 describe("GET /teachers:id", ()=>{
+
 	it("given no token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/teachers/1")
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Missing authorization heander'})
 	})
 
 	it("given wrong token request should be rejected, error 401",async()=>{
 		const response = await supertest(app).get("/teachers/1").set('Authorization', `Bearer asd`)
 		expect(response.status).toBe(401)
+		expect({name: 'unauthorizedError', message: 'Invalid token'})
 	})
 
 	it("given right token but id params is NaN request should be rejected, error 400",async()=>{
@@ -208,11 +226,13 @@ describe("GET /teachers:id", ()=>{
 	it("given right token but id params doesnt exist request should return error 404",async()=>{
 		const response = await supertest(app).get("/teachers/1000").set('Authorization', `Bearer ${globalToken}`)
 		expect(response.status).toBe(404)
+		expect({name: 'notFoundError', message: 'Teachers not found for given discipline'})
 	})
 
 	it("given right token and existing id params request should return teachers of given discipline, status 200",async()=>{
 		const response = await supertest(app).get("/teachers/1").set('Authorization', `Bearer ${globalToken}`)
 		expect(response.status).toBe(200)
+		expect(response.body.teachers.length).toBeGreaterThan(0)
 	})
 	afterAll(async() => {
 		await prisma.$disconnect();
